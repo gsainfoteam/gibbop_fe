6b7d90ce8836f0b6d3617585b959fdea6c21d949
siwonpada <psw041115@gmail.com>
2023-02-28 02:53:50 +0900

    route setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import InitialSetting from './Component/InitialSetting';
+import Login from './Component/Login';
+import MealPlanner from './Component/MealPlanner';
 
-function App() {
+function App(): JSX.Element {
   return (
-    <div>
-      Hello, GiBBOP!
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MealPlanner />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/initial_setting" element={<InitialSetting />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Component/MealPlanner.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MealPlanner = (): JSX.Element => {
+  return <>Planner!!</>;
+};
+
+export default MealPlanner;
